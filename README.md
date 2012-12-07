dbad983f279bb6c0310d3d515560e9aefbd9fcfc
Superjd10 <superjd10@superjd10.x10.bz>
2012-12-06 21:10:58 -0500

    Agregando archivos aprendidos de la clase de españa con el logo y nombre de la de LATAM, se aceptan mejoras y ayudas

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 -------------------------------
 Lo que aprendimos en la clase de #mejorandocurso los de latinoamerica.
 El logo fue uno enviado por un compañero en el chat y no el que @freddier usó.
+Agradezo a @geninteractive por los apuntes ubicados en "CursodeHTML5Mejorandola.la.html"
---
+++ codiguito.js
@@ -0,0 +1,29 @@
+$(document).on("ready", inicio);
+function inicio ()
+{
+	$("#personalizar").on("click", transicion);
+}
+function transicion ()
+{
+	//objeto javascript: JSON
+	var cambios = {
+		margin: 0,
+		overflow: "hidden",
+		padding: 0,
+		width: 0
+	};
+	var cambios2 = {
+		overflow: "visibility",
+		display: "inline-block"
+	};
+	$("#historia").css(cambios);
+	$("#personalizacion").css(cambios2);
+	$("#listanav").html('<li><a href="#" id="cancelar">Cancelar</a></li>');
+	$("#color div").on("click", cambiarColor);
+}
+function cambiarColor (datos) 
+{
+	var col = datos.currentTarget.id;
+	$("#cochecito img").attr("src", "c"+ col +".jpg");
+	$("#AutoColor").html(col);
+}
